0683790a20e1ca24bb44329cb27c51054840fc22
orenciorodolfo <orenciorodolfo@hotmail.com>
2017-09-24 17:45:03 +0100

    Add hot module replacement

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -3,3 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+if (module.hot) {
+  console.log('aqui');
+  module.hot.accept('./components/App.js', () => {
+    const NextApp = require('./components/App.js').default;
+    ReactDOM.render(<NextApp />, document.getElementById('root'));
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,31 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const src = './src/client';
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: `${src}/index.html`,
-  filename: 'index.html',
-  inject: 'body',
-});
-
 
 module.exports = {
-  entry: `${src}/js/index.js`,
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    `${src}/js/index.js`,
+  ],
   output: {
     path: path.resolve('public/js'),
     filename: 'app.js',
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.js$/,
+        loader: ['babel-loader'],
+        exclude: /node_modules/,
+      },
     ],
   },
-  plugins: [HtmlWebpackPluginConfig],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `${src}/index.html`,
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
 };
