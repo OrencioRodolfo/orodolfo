d7b78d8a75c027c6cd4573f7f5eb07f7cd171dbf
orenciorodolfo <orenciorodolfo@hotmail.com>
2017-09-29 23:36:53 +0100

    Remove dead code

--- src/client/js/components/Icon/Icon.js
+++ src/client/js/components/Icon/Icon.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const I = styled.i`
-`;
-
 const Button = ({ icon }) => (
-  <I className={`fa fa-${icon}`} />
+  <i className={`fa fa-${icon}`} />
 );
 
 Button.propTypes = {
