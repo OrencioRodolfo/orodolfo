0b12245cd9ae514606211dfc566a4a5ebeb4e135
orenciorodolfo <orenciorodolfo@hotmail.com>
2017-09-30 21:28:04 +0100

    Change server's config
    DB connection configurations

--- src/server/config.mjs
+++ src/server/config.mjs
@@ -3,5 +3,8 @@ export default {
     host: 'localhost',
     port: '27017',
     name: 'orodolfo',
+    get url() {
+      return process.env.MONGODB_URI || `mongodb://${this.host}:${this.port}/${this.name}`;
+    },
   },
 };
--- src/server/controllers/about.mjs
+++ src/server/controllers/about.mjs
@@ -2,11 +2,10 @@ import Mongo from 'mongodb';
 import config from '../config.mjs';
 
 const MongoClient = Mongo.MongoClient;
-const url = `mongodb://${config.db.host}:${config.db.port}/${config.db.name}`;
 
 function get(req, res) {
   // Use connect method to connect to the server
-  MongoClient.connect(url, (err, db) => {
+  MongoClient.connect(config.db.url, (err, db) => {
     const collection = db.collection('about');
     // Find some documents
     collection.findOne({}, (error, doc) => {
