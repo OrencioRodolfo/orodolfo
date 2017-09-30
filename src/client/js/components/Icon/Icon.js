fab221026e88cf1ec2d4bb78cb0cc06e5d4a0ef4
orenciorodolfo <orenciorodolfo@hotmail.com>
2017-09-30 12:12:54 +0100

    Remove "cursor" pointer from icon component
    Link must be responsible for that

--- src/client/js/components/Icon/Icon.js
+++ src/client/js/components/Icon/Icon.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
-
-const I = styled.i`
-  cursor: pointer;
-`;
 
 const Button = ({ icon }) => (
-  <I className={`fa fa-${icon}`} />
+  <i className={`fa fa-${icon}`} />
 );
 
 Button.propTypes = {
