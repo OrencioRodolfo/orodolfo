e6bf89bf858edb327c1598a4a2448698b019c9b0
orenciorodolfo <orenciorodolfo@hotmail.com>
2017-09-30 01:03:16 +0100

    Add "cursor: pointer" to all the icons

--- src/client/js/components/Icon/Icon.js
+++ src/client/js/components/Icon/Icon.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const I = styled.i`
+  cursor: pointer;
+`;
 
 const Button = ({ icon }) => (
-  <i className={`fa fa-${icon}`} />
+  <I className={`fa fa-${icon}`} />
 );
 
 Button.propTypes = {
