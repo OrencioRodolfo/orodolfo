14afa4312b7276adae755e11c0226cc94e4d4cab
orenciorodolfo <orenciorodolfo@hotmail.com>
2017-09-30 20:57:41 +0100

    Add MongoDB connection
    And retrieve the "about" information from the db

--- src/server/app.mjs
+++ src/server/app.mjs
@@ -1,13 +1,15 @@
 import express from 'express';
-import routes from './routes';
+
+
+import routes from './routes.mjs';
 
 const app = express();
+
 
 app.set('port', (process.env.PORT || 3000));
 
 app.use('/', express.static(`${process.cwd()}/public`));
-
-app.get('/', (req, res) => res.sendFile(`${process.cwd()}/index.html`));
+app.get('/', (req, res) => res.sendFile(`${process.cwd()}/../index.html`));
 
 // load routes
 routes(app);
--- src/server/controllers/about.mjs
+++ src/server/controllers/about.mjs
@@ -1,5 +1,20 @@
+import Mongo from 'mongodb';
+import config from '../config.mjs';
+
+const MongoClient = Mongo.MongoClient;
+const url = `mongodb://${config.db.host}:${config.db.port}/${config.db.name}`;
+
 function get(req, res) {
-  res.send('----');
+  // Use connect method to connect to the server
+  MongoClient.connect(url, (err, db) => {
+    const collection = db.collection('about');
+    // Find some documents
+    collection.findOne({}, (error, doc) => {
+      res.json(doc);
+    });
+
+    db.close();
+  });
 }
 
 export default {
