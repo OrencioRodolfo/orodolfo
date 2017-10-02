637f73fac604fef03b0aa186533f4549515abeba
orenciorodolfo <orenciorodolfo@hotmail.com>
2017-10-02 21:56:15 +0100

    Update snapshots and add redux-mock-store
    To mock our store

--- src/client/js/components/App/App.js
+++ src/client/js/components/App/App.js
@@ -2,13 +2,8 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
-// import { createStore } from 'redux';
-// import reducers from '../../reducers';
 import * as theme from '../../theme';
-
 import Home from '../../scenes/Home/components/Home/Home';
-
-// const store = createStore(reducers);
 
 const App = props => (
   <Provider store={props.store}>
--- src/client/js/components/App/App.test.js
+++ src/client/js/components/App/App.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
 import App from './App';
 
 describe('App component', () => {
+  const middlewares = [];
+  const mockStore = configureStore(middlewares);
+
   it('Must render properly', () => {
+    const store = mockStore({});
+
     const container = shallow(
-      <App />,
+      <App store={store} />,
     );
 
     expect(container).toMatchSnapshot();
