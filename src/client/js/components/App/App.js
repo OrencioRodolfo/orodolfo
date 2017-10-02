1d3fa4bf201123a547e9ec6fb364bffbeb3a5c76
orenciorodolfo <orenciorodolfo@hotmail.com>
2017-10-02 21:14:29 +0100

    Fix hot reload with redux store

--- src/client/js/components/App/App.js
+++ src/client/js/components/App/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
+// import reducers from '../../reducers';
 import * as theme from '../../theme';
-import reducers from '../../reducers';
 
 import Home from '../../scenes/Home/components/Home/Home';
 
-const store = createStore(reducers);
+// const store = createStore(reducers);
 
-const App = () => (
-  <Provider store={store}>
+const App = props => (
+  <Provider store={props.store}>
     <ThemeProvider theme={theme}>
       <Home />
     </ThemeProvider>
   </Provider>
 );
+
+App.propTypes = {
+  store: PropTypes.any.isRequired,
+};
 
 export default App;
