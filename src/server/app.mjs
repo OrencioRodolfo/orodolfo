bc873bd08037e7e4be57e47d9668813053e9be91
Rodolfo Gonçalves <OrencioRodolfo@users.noreply.github.com>
2017-10-12 12:34:33 +0100

    Add headers to the response

--- src/server/app.mjs
+++ src/server/app.mjs
@@ -10,8 +10,8 @@ app.use(cors());
 
 // Add headers
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
