02f21a4b90e69f93b39a4e1e741c2243a493d439
orenciorodolfo <orenciorodolfo@hotmail.com>
2017-10-14 11:34:15 +0100

    Add google analytics script

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -28,15 +28,21 @@ if (module.hot) {
 }
 
 if (ENV_PRODUCTION) {
-  // Hotjar Tracking Code for www.orodolfo.com
   /*eslint-disable */
+  // Hotjar Tracking Code for www.orodolfo.com
   (function(h,o,t,j,a,r){
-      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-      h._hjSettings={hjid:656483,hjsv:6};
-      a=o.getElementsByTagName('head')[0];
-      r=o.createElement('script');r.async=1;
-      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-      a.appendChild(r);
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:656483,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
   })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+
+  // Google analytics
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-56126854-3');
   /*eslint-enable */
 }
